c0418cf93641cf60ca37d3a6763fa712fca0cf66
DeaSTL <jhartway99@gmail.com>
2019-11-13 15:02:11 -0500

    Added forge files

---
+++ src/main/java/com/stryphic/deepsouthmod/DeepSouthMod.java
@@ -0,0 +1,30 @@
+package com.stryphic.deepsouthmod;
+
+
+import com.stryphic.deepsouthmod.util.Reference;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.Logger;
+
+@Mod(modid = Reference.MODID, name = Reference.NAME, version = Reference.VERSION)
+public class DeepSouthMod
+{
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event)
+    {
+        logger = event.getModLog();
+    }
+
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+        // some example code
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
---
+++ src/main/java/com/stryphic/deepsouthmod/util/Reference.java
@@ -0,0 +1,7 @@
+package com.stryphic.deepsouthmod.util;
+
+public class Reference {
+    public static final String MODID = "deepsouthmod";
+    public static final String NAME = "Deep South Mod";
+    public static final String VERSION = "0.0.1";
+}
